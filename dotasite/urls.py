27673846c8feff3c1a3e4b2b3147144222420e26
G O <GO@GO>
2014-05-20 01:04:19 -0700

    Simple update to get the site actually working. Root URL is functional.

--- dotasite/urls.py
+++ dotasite/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from . import views
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,5 +10,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'dotasite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', views.index, name="index"),
     url(r'^admin/', include(admin.site.urls)),
 )
