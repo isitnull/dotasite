53a140910f3072cb7918c408914da2f390babdd2
G O <gary21490@gmail.com>
2014-05-25 23:48:29 -0700

    Addedw leaderboards views to the urls for the project.

--- dotasite/urls.py
+++ dotasite/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns(
     url(r'^login/', views.auth_login, name="login"),
     url(r'^logout/', views.auth_logout, name="logout"),
     url(r'^challenge/', views.challenge, name="challenge"),
+    url(r'^leaderboard/', include("leaderboard.urls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', views.index, name="index"),
 )
